f6f6ed373e04b4850b93e866e107f62877c9cfad
rizwanwasim <rizwanwasim115@gmail.com>
2024-02-14 03:00:22 +0500

    add to cart in loop

--- src/ui/test/main.test.ts
+++ src/ui/test/main.test.ts
@@ -20,7 +20,36 @@ test.describe("Main Page", () => {
     // await page.waitForTimeout(3000);
     await LoginUser(page, process.env.STANDARD, process.env.PASSWORD);
     await mainPageFieldsVisible(mainPage, page);
+    // Iterate over the four products on the page
+    for (let i = 1; i <= 4; i++) {
+      // Locate the product name
+      const productNameLocator = `[id='item_${i}_title_link']`;
+      const productNameElement = await page.waitForSelector(productNameLocator);
 
-    //   await mainPage.myProjectsTab.click();
+      // Extract the product name text and modify it
+      let productNameText = (await productNameElement.textContent())?.trim();
+
+      // Ensure productNameText is not undefined before proceeding
+      if (productNameText !== undefined) {
+        productNameText = productNameText.replace(/\s+/g, "-").toLowerCase();
+
+        // Locate the "Add to Cart" button using the modified product name
+        const addToCartButtonLocator = `[data-test="add-to-cart-${productNameText}"]`;
+        const addToCartButton = await page.waitForSelector(
+          addToCartButtonLocator
+          // { visible: true }
+        );
+
+        // Scroll the button into view if needed
+        await addToCartButton.scrollIntoViewIfNeeded();
+
+        // Click on the "Add to Cart" button
+        await addToCartButton.click();
+
+        console.log(`Added ${productNameText} to cart.`);
+      } else {
+        console.log(`Failed to retrieve product name for product ${i}.`);
+      }
+    }
   });
 });
